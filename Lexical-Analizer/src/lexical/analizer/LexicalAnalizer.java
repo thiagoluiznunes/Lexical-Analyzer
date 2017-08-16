071da0438f01b1a92ff67981e469dab5940f924a
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2017-08-15 21:51:55 -0300

    token class initialized with your attributes

--- Lexical-Analizer/src/lexical/analizer/LexicalAnalizer.java
+++ Lexical-Analizer/src/lexical/analizer/LexicalAnalizer.java
@@ -15,7 +15,8 @@ public class LexicalAnalizer {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        
     }
     
 }
---
+++ Lexical-Analizer/src/lexical/analizer/Tokens.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lexical.analizer;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author thiago
+ */
+public class Tokens {
+    
+    protected String[] keyWords = {"program", "var", "integer", "real", "boolean", "procedure", "begin",
+                                "end", "if", "then", "else", "while", "do", "not"};
+    
+    protected String identifiers = "([a-z]|[A-Z])([0-9]|[a-z]+|[A-Z]|_)*";
+    protected String[] delimiters = {";",".",":","(",")",","};
+    protected String attributtion = ":=";
+    protected String[] operatiorsR = {"=","<",">","<=",">=","<>"};
+    protected String[] operatiorsA = {"+","-","or"};
+    protected String[] operatiorsM = {"*","/","and"};
+    
+    protected String intDigits = "[0-9]+";
+    protected String floatDigits = "[0-9]+[.][0-9]+";
+    protected String[] booleanDigits = {"true", "false"};
+
+    public Tokens() {
+    }
+    
+    
+    
+}
